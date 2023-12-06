e86db398fcb03d64009f35f327ae9b6e280d4fe3
Daniela Andrea Poveda <d.poveda@redlink.com.ar>
2023-12-05 22:00:45 -0300

    ej 2 ok tsql

--- Practica T-SQL/ejercicio 2 resuelto.sql
+++ Practica T-SQL/ejercicio 2 resuelto.sql
@@ -1,25 +1,40 @@
---EJERCICIO 2
-
-/*Realizar una función que dado un artículo y una fecha, retorne el stock que
+/*2. Realizar una función que dado un artículo y una fecha, retorne el stock que
 existía a esa fecha*/
 
 --creo la funcion
-create function stock_segun_fecha(@producto char(8), @fecha date)
+create function stockSegunFecha5(@articulo char(8), @fecha smalldatetime)
 returns decimal(12,2)
-as
 begin
-return
-	(
-	select isnull(sum(i.item_cantidad),0) + 
-		(select isnull(sum(stoc_cantidad),0)
-		from stock
-		where stoc_producto=@producto)
-	from Item_Factura i 
-		join Factura f ON f.fact_tipo+f.fact_sucursal+f.fact_numero = i.item_tipo+i.item_sucursal+i.item_numero
-	where i.item_producto=@producto and f.fact_fecha <= @fecha 
-	)
+	DECLARE @stock decimal(12,2)
+
+	set @stock = (select SUM(distinct s.stoc_cantidad)
+					from stock s
+						join Producto p on s.stoc_producto = p.prod_codigo
+						join Item_Factura i on p.prod_codigo = i.item_producto
+						join Factura f on i.item_numero+i.item_sucursal+i.item_tipo = f.fact_numero+f.fact_sucursal+f.fact_tipo
+				where p.prod_codigo = @articulo and f.fact_fecha = @fecha
+				)
+
+	return @stock
 end
 
 
---llamo a la funcion 
-select stoc_producto, dbo.stock_segun_fecha(stoc_producto, '01/01/2012') from stock
+--ejecuto la funcion
+select p.prod_codigo, f.fact_fecha, dbo.stockSegunFecha5(p.prod_codigo,f.fact_fecha)
+from stock s
+join Producto p on s.stoc_producto = p.prod_codigo
+join Item_Factura i on p.prod_codigo = i.item_producto
+join Factura f on i.item_numero+i.item_sucursal+i.item_tipo = f.fact_numero+f.fact_sucursal+f.fact_tipo
+group by p.prod_codigo, f.fact_fecha
+order by f.fact_fecha
+
+
+
+/*
+select SUM( s.stoc_cantidad)
+from stock s
+join Producto p on s.stoc_producto = p.prod_codigo
+join Item_Factura i on p.prod_codigo = i.item_producto
+join Factura f on i.item_numero+i.item_sucursal+i.item_tipo = f.fact_numero+f.fact_sucursal+f.fact_tipo
+where p.prod_codigo = '00010104' and f.fact_fecha = '2012-04-30 00:00:00' 
+*/
