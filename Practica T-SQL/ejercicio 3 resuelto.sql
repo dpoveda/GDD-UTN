2ae5c87630232d60f8f6d283fac31437808cb006
Daniela Andrea Poveda <d.poveda@redlink.com.ar>
2023-12-08 04:04:30 -0300

    ej 3 ok

--- Practica T-SQL/ejercicio 3 resuelto.sql
+++ Practica T-SQL/ejercicio 3 resuelto.sql
@@ -1,6 +1,4 @@
---EJERCICIO 3
-
-/*Cree el/los objetos de base de datos necesarios para corregir la tabla empleado
+/*3. Cree el/los objetos de base de datos necesarios para corregir la tabla empleado
 en caso que sea necesario. Se sabe que debería existir un único gerente general
 (debería ser el único empleado sin jefe). Si detecta que hay más de un empleado
 sin jefe deberá elegir entre ellos el gerente general, el cual será seleccionado por
@@ -11,20 +9,85 @@ de empleados que había sin jefe antes de la ejecución.*/
 
 
 
-/*Asumo que no es una funcion pues modifica una tabla, tampoco un trigger pues no hay ningun evento que lo desencadene, 
-por lo tanto es un SP*/
-
-create procedure ej3 @cantidad int output --declaro la variable cantidad que voy a retornar despues
-as
+--SP REALIZADO
+alter procedure sp_update_empleados_un_solo_gerente
+@cant_empl_sin_jefe decimal(10, 0) output
+as 
 begin
---muestro la cantidad de empleados que no tenian jefe antes de corregir la tabla
-set @cantidad = (select count(*) from Empleado where empl_jefe is null)
+	set @cant_empl_sin_jefe = (select dbo.cantEmpleadoSinJefe())
 
---actualizo la tabla empleado dejando solamente un gerente general, y a los demas, le pongo como jefe ese gerente general que encontre
-update Empleado set empl_jefe = (select top 1 empl_codigo from Empleado where empl_jefe is null order by empl_salario desc, empl_ingreso asc)
-from Empleado 
-where empl_jefe is null and empl_codigo <> (select top 1 empl_codigo from Empleado where empl_jefe is null order by empl_salario desc, empl_ingreso asc)
+	if @cant_empl_sin_jefe > 1 
+		begin
+			declare @cod_gerente numeric(6,0)
+	
+			set @cod_gerente = (select dbo.seleccionarGerenteGeneral())
 
-return --pongo el return pues quiero retornar la cantidad
+			update empleado 
+			set empl_tareas = 'Gerente'
+			where empl_codigo = @cod_gerente
+
+			update Empleado
+			set empl_jefe = @cod_gerente
+			where empl_codigo != @cod_gerente and empl_jefe is null
+
+		end
+	
 end
+
+
+
+--retorno del sp que utiliza las funciones
+
+------------------------------
+DECLARE @ValorResultado decimal(10, 0);
+
+-- Ejecutar el procedimiento almacenado y obtener el resultado
+EXEC sp_update_empleados_un_solo_gerente @cant_empl_sin_jefe = @ValorResultado OUTPUT;
+
+-- Utilizar el valor obtenido
+PRINT @ValorResultado;
+
+
+----------------------------------------------------
+
+--FUNCIONES REALIZADAS
+
+ALTER function cantEmpleadoSinJefe()
+returns decimal (10, 0)
+begin
+	DECLARE @cantidad decimal (10, 0)
+	set @cantidad = 
+		(select 
+			count(*)
+		from empleado 
+		where empl_jefe is null
+		)
+	return @cantidad
+end
+
+
+ALTER function seleccionarGerenteGeneral()
+returns numeric(6,0)
+begin
+	declare @cod_empleado numeric(6,0)
+
+	set @cod_empleado = 
+		(select top 1 empl_codigo
+		from empleado
+		order by empl_salario desc, empl_ingreso)
+
+	return @cod_empleado
+end
+
+
+
+----------------------------------------------------
+
+
+--Para hacer pruebas
+update empleado 
+	set empl_jefe = NULL
+	where empl_codigo = 2
+
+SELECT * FROM Empleado
 
